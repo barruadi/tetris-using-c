b5285a7d4d615de4d53b749a71c3c7510de2cbd9
Barru Adi <barru.adi@gmail.com>
2025-02-08 19:23:26 +0700

    feat: disable and enable terminal buffer

---
+++ utils/utils.c
@@ -0,0 +1,32 @@
+#include "utils.h"
+
+InputKeyErrno utils_enable_raw_mode() {
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    return INPUT_KEY_SUCCESS;
+}
+
+InputKeyErrno utils_kbhit() {
+    struct timeval tv = {0, 0};
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+    if (select(STDIN_FILENO + 1, &readfds, NULL, NULL, &tv) == 1) {
+        return INPUT_KEY_SUCCESS;
+    } else if (select(STDIN_FILENO + 1, &readfds, NULL, NULL, &tv) == -1) {
+        return INPUT_KEY_ERR_NO_KEY;
+    }
+    return INPUT_KEY_ERR_UNKNOWN_KEY;
+}
+
+InputKeyErrno utils_get_key_press(char *key) {
+    char c;
+    if (utils_kbhit() == INPUT_KEY_SUCCESS) {
+        read(STDIN_FILENO, &c, 1);
+        *key = c;
+        return INPUT_KEY_SUCCESS;
+    }
+    return INPUT_KEY_ERR_NO_KEY;
+}
--- utils/utils.h
+++ utils/utils.h
@@ -4,6 +4,9 @@
 #include "boolean.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/select.h>
 
 // Terminal control
 #define CLEARSCREEN() printf("\033[H\033[J")
@@ -16,5 +19,21 @@
         exit(EXIT_FAILURE); \
     }
 
+typedef enum {
+    INPUT_KEY_SUCCESS = 0,
+    INPUT_KEY_ERR_NO_KEY = 1,
+    INPUT_KEY_ERR_UNKNOWN_KEY = 2,
+    INPUT_KEY_ERR_INVALID_KEY = 3
+} InputKeyErrno;
+
+
+// Terminal control functions
+InputKeyErrno utils_enable_raw_mode();
+
+InputKeyErrno utils_kbhit();
+
+InputKeyErrno utils_get_key_press(char *key);
+
+InputKeyErrno utils_disable_raw_mode();
 
 #endif
